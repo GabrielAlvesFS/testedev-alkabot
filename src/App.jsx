9e9e9121bda23a498b8f803eac4915069267f026
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-24 10:53:07 -0300

    :sparkles: feat: configuring routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+
 const App = () => (
-  <h1>Alkabot Social</h1>
+  <>
+    <Header />
+    <Routes>
+      <Route exact path="/posts" />
+      <Route exact path="/users" />
+    </Routes>
+  </>
 );
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import GlobalStyles from './GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <Router>
+      <GlobalStyles />
+      <App />
+    </Router>
   </React.StrictMode>,
 );
