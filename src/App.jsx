19da0825add03e28b190c83803d30f492f5639bd
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-27 13:46:09 -0300

    :sparkles: feat: adding component into post route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
+import { Posts } from './components/Posts';
 
 const App = () => (
   <>
     <Header />
     <Routes>
-      <Route exact path="/posts" />
+      <Route exact path="/" element={<Posts />} />
+      <Route exact path="/posts" element={<Posts />} />
       <Route exact path="/users" />
     </Routes>
   </>
