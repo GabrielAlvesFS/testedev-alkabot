20714a1b6cd20cc4ad86f564fd41a05336bee63e
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-27 17:27:57 -0300

    ✨ feat: adding component into user route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Posts } from './components/Posts';
+import { Users } from './components/Users';
 
 const App = () => (
   <>
@@ -8,7 +9,7 @@ const App = () => (
     <Routes>
       <Route exact path="/" element={<Posts />} />
       <Route exact path="/posts" element={<Posts />} />
-      <Route exact path="/users" />
+      <Route exact path="/users" element={<Users />} />
     </Routes>
   </>
 );
