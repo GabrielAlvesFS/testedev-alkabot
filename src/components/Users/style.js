e549ba65f8cc26c0bc21518db84de3610ebfe2c3
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-27 17:25:18 -0300

    :lipstick: feat: users stylization

---
+++ src/components/Users/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const UsersContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 50px;
+
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  margin-top: 30px;
+`;
